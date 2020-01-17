c2af93371877fcdfaeeee30627eba95f55e79489
Natal <natanaelweslley1@gmail.com>
2020-01-17 20:44:19 +0000

    Conexão com banco de dados~

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb+srv://omnistack:omnistack10@devradar-t7vfb.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.use(express.json());
+
 app.get('/', (request, response) => {
-    return response.send('Hello Word');
+    return response.json({ message: 'Hello Word'});
 });
 
 app.listen(3333);
