1572930f4ec6d402406c32522937a8ae0c51cbb5
Natal <natanaelweslley1@gmail.com>
2020-01-20 20:08:09 +0000

    Salvando Devs no BD

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,26 @@
 const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-routes.get('/devs', (request, response) => {
-    return response.json({ message: 'Hello Word'});
+routes.post('/devs', async (request, response) => {
+    const { github_username, techs } = request.body;
+
+    const gitResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    const { name = login, avatar_url, bio } = gitResponse.data;
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+   const dev = await Dev.create({
+       github_username,
+       name,
+       avatar_url,
+       bio,
+       techs: techsArray,
+   });
+
+    return response.json(dev);
 });
 
 module.exports = routes;
-
