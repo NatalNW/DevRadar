54a5233cf2279e9217dc9a7a892c88e55827e9b7
Natal <natanaelweslley1@gmail.com>
2020-01-20 20:40:34 +0000

    Resolvendo bugs

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,11 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack10@devradar-t7vfb.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://omnistack:omnistack11@devradar-t7vfb.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
+    useCreateIndex: true,
+});
 
 app.use(express.json());
 app.use(routes);
